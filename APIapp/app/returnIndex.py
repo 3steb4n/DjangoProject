1a71c214128c8c21eedf782053cc243be4eb7c7c
Ludenverg <ludenverg@gmail.com>
2020-08-17 18:15:57 -0400

    Se cambio el servicio a Getbalance

--- APIapp/app/returnIndex.py
+++ APIapp/app/returnIndex.py
@@ -14,13 +14,14 @@ def returnIndex(request):
     apiParameters = {
         'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
     }
-    responseRe = generate_request('https://faucetpay.io/api/v1/currencies', apiParameters)
+    responseRe = generate_request('https://faucetpay.io/api/v1/getbalance', apiParameters)
 
     if responseRe:
         statusService = {
             "status" : responseRe.get('status'),
             "message" : responseRe.get('message'),
-            "currencies" : responseRe.get('currencies')
+            "currencies" : responseRe.get('currency'),
+            "balance" : responseRe.get('balance')
         }
 
     return render(request, 'index.html', statusService) #Anotar la ruta completa en settings/TEMPLATES - DIRS
