aafa4561ac5efaa574870b2d38530bbc13a89fed
3steb4n <juangomez1947@outlook.com>
2020-08-17 16:25:25 -0500

    Se modifica el tipo de invocacion al metodo POST

--- APIapp/app/returnIndex.py
+++ APIapp/app/returnIndex.py
@@ -5,13 +5,16 @@ import requests
 
 
 def generate_request(url, auth):
-    response = requests.get(url, auth)
+    response = requests.post(url, auth)
 
     if response.status_code == 200:
         return response.json()
 
 def returnIndex(request):
-    responseRe = generate_request('https://faucetpay.io/api/v1/currencies', '95e72c3491c419b628e86626d39709d265ae91cd')
+    apiParameters = {
+        'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
+    }
+    responseRe = generate_request('https://faucetpay.io/api/v1/currencies', apiParameters)
 
     if responseRe:
         statusService = {
