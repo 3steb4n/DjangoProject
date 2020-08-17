6c7a02ec6373be0f8dd1b4692c2466d96e2fa9c7
3steb4n <juangomez1947@outlook.com>
2020-08-17 16:00:19 -0500

    Se agrega funciones para el consumo de una api

--- APIapp/app/returnIndex.py
+++ APIapp/app/returnIndex.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+import requests
 
-def returnIndex(request):
-    informationApi = {}
-    return render(request, 'index.html', informationApi) #Anotar la ruta completa en settings/TEMPLATES - DIRS
+
+def generate_request(url, params={}):
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+
+def returnIndex(request, params={
+    'api_key' : '5b8a92b6414f88d358bc39e8db7de2fef64417b6'
+
+}):
+    responseRe = generate_request('https://faucetpay.io/api/v1/currencies', params)
+
+    if responseRe:
+        statusService = {
+            "context" : responseRe.get('status')
+        }
+
+    return render(request, 'index.html', statusService) #Anotar la ruta completa en settings/TEMPLATES - DIRS
