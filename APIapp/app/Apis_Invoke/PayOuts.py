75b3a92e3120f4402cbf026b30b92b0b95d54e17
3steb4n <juangomez1947@outlook.com>
2020-08-18 13:07:23 -0500

    Se actualiza parametros del servicios

--- APIapp/app/Apis_Invoke/PayOuts.py
+++ APIapp/app/Apis_Invoke/PayOuts.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 import requests
-from BaseFile import generate_request
+from APIapp.app.Apis_Invoke.BaseFile import generate_request
 
 def returnPayOut(request):
     apiParameters = {
-        'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
+        'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd', #Parametro obligatorio
+        'currency' : '', #Parametro opcional
+        'count' : '' #Parametro opcional
     }
     responseRe = generate_request('https://faucetpay.io/api/v1/payouts', apiParameters)
 
@@ -14,7 +16,6 @@ def returnPayOut(request):
         responseServicePayOuts = {
             "status" : responseRe.get('status'),
             "message" : responseRe.get('message'),
-            "currencies" : responseRe.get('currency'),
-            "balance" : responseRe.get('balance')
+            "rewards_to" : responseRe.get('rewards')
         }
     return render(request, 'PayOuts.html', responseServicePayOuts)
