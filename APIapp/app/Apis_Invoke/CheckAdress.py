e2b6cb44fed74e8585350226714b978403beb1b8
3steb4n <juangomez1947@outlook.com>
2020-08-18 11:41:55 -0500

    Se coloca la redirreccion de html

--- APIapp/app/Apis_Invoke/CheckAdress.py
+++ APIapp/app/Apis_Invoke/CheckAdress.py
@@ -1,18 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 import requests
 from BaseFile import generate_request
 
-def returnCheckAdress():
+def returnCheckAdress(request):
     apiParameters = {
         'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
     }
     responseRe = generate_request('https://faucetpay.io/api/v1/checkaddress', apiParameters)
 
     if responseRe:
-        responseService = {
+        responseServiceCheckAdress = {
             "status" : responseRe.get('status'),
             "message" : responseRe.get('message'),
             "currencies" : responseRe.get('currency'),
             "balance" : responseRe.get('balance')
         }
-
-    print(responseService)
+    return render(request, 'CheckAdress.py', responseServiceCheckAdress)
