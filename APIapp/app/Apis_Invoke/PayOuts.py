c4b8cf390f4e39c9612ec35abffff1c674114add
3steb4n <juangomez1947@outlook.com>
2020-08-18 11:41:20 -0500

    Se coloca la redirreccion de html

--- APIapp/app/Apis_Invoke/PayOuts.py
+++ APIapp/app/Apis_Invoke/PayOuts.py
@@ -1,18 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 import requests
 from BaseFile import generate_request
 
-def returnPayOut():
+def returnPayOut(request):
     apiParameters = {
         'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
     }
     responseRe = generate_request('https://faucetpay.io/api/v1/payouts', apiParameters)
 
     if responseRe:
-        responseService = {
+        responseServicePayOuts = {
             "status" : responseRe.get('status'),
             "message" : responseRe.get('message'),
             "currencies" : responseRe.get('currency'),
             "balance" : responseRe.get('balance')
         }
-
-    print(responseService)
+    return render(request, 'PayOuts.html', responseServicePayOuts)
