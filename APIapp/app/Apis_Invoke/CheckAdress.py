5d73371c1d764f39ed51afafee09e01632720845
3steb4n <juangomez1947@outlook.com>
2020-08-18 13:00:59 -0500

    Se realiza cambio en la invocación de librerias

--- APIapp/app/Apis_Invoke/CheckAdress.py
+++ APIapp/app/Apis_Invoke/CheckAdress.py
@@ -2,19 +2,29 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 import requests
-from BaseFile import generate_request
+from APIapp.app.Apis_Invoke.BaseFile import generate_request
 
 def returnCheckAdress(request):
     apiParameters = {
-        'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd'
+        'api_key' : '95e72c3491c419b628e86626d39709d265ae91cd', #Parametro requerido
+        'address' : '', #Parametro requerido
+        'currency' : '' #Parametro opcional
     }
+    
     responseRe = generate_request('https://faucetpay.io/api/v1/checkaddress', apiParameters)
 
     if responseRe:
-        responseServiceCheckAdress = {
+        if responseRe.get('status') != '200':
+            responseServiceCheckAdress = {
+            "responseRe" : responseRe.get('status'),
+            "status" : responseRe.get('status'),
+            "message" : responseRe.get('message')
+            }
+        else:
+            responseServiceCheckAdress = {
+            "responseRe" : responseRe.get('status'),
             "status" : responseRe.get('status'),
             "message" : responseRe.get('message'),
-            "currencies" : responseRe.get('currency'),
-            "balance" : responseRe.get('balance')
-        }
-    return render(request, 'CheckAdress.py', responseServiceCheckAdress)
+            "payout_user_hash" : responseRe.get('payout_user_hash')
+            }
+    return render(request, 'CheckAdress.html', responseServiceCheckAdress)
